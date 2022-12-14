95c4142165152b17329c93a1c1e90918f0008d65
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2022-12-14 13:51:20 -0500

    #1 - Django model class stubs

---
+++ peer-review-data/models.py
@@ -0,0 +1,26 @@
+# standard libaries
+import logging
+from datetime import datetime
+from typing import Union
+
+# third-party libraries
+from django.db import models
+from django.db.models.constraints import BaseConstraint
+
+
+LOGGER = logging.getLogger(__name__)
+
+class Assessment(models.Model):
+    id = models.IntegerField(primary_key=True, verbose_name='Assessment ID')
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True, verbose_name='User ID')
+
+class Assignment(models.Model):
+    id = models.IntegerField(primary_key=True, verbose_name='Assignment ID')
+
+class Rubric(models.Model):
+    id = models.IntegerField(primary_key=True, verbose_name='Rubric ID')
+
+class Submission(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='Submission ID')
