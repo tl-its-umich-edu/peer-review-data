d572bb63c9acc27e80cf8e77b6a36f80c5b6dfd1
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2022-12-21 14:08:45 -0500

    #1 - added logging and input validation

--- canvasData.py
+++ canvasData.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
+from logging import Logger, getLogger
 
 from canvasapi import Canvas
 from canvasapi.assignment import Assignment
 from canvasapi.course import Course
 from canvasapi.rubric import Rubric
+
+LOGGER: Logger = getLogger(__name__)
 
 CANVAS_API_URL: str = os.getenv('CANVAS_API_URL',
                                 'https://canvas-test.it.umich.edu/')
@@ -12,14 +16,29 @@ CANVAS_API_TOKEN: str = os.getenv('CANVAS_API_TOKEN')
 COURSE_ID: str = os.getenv('COURSE_ID')
 ASSIGNMENT_ID: str = os.getenv('ASSIGNMENT_ID')
 
+envErrors = []
+
 if CANVAS_API_TOKEN is None:
-    raise RuntimeError('"CANVAS_API_TOKEN" was not set.')
+    envErrors.append('CANVAS_API_TOKEN')
+
+if COURSE_ID is None:
+    envErrors.append('COURSE_ID')
+
+if ASSIGNMENT_ID is None:
+    envErrors.append('ASSIGNMENT_ID')
+
+if len(envErrors) > 0:
+    LOGGER.critical('The following environment variable(s) are not set: '
+                    f'{", ".join(envErrors)}')
+    sys.exit()
 
 canvas = Canvas(CANVAS_API_URL, CANVAS_API_TOKEN)
+
 
 class CanvasCourse(Course):
     id: int
     name: str
+
 
 class CanvasAssignment(Assignment):
     id: int
