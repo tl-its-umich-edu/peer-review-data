e1f07e0144a730258c4b0a0bb025a82f70e7c56a
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2022-12-21 14:12:17 -0500

    #1 - added Django manage.py

---
+++ manage.py
@@ -0,0 +1,22 @@
+# standard libraries
+import logging
+import os
+import sys
+
+# third-party libraries
+from dotenv import load_dotenv
+
+CONFIG_DIR: str = os.getenv('ENV_DIR', os.path.join('config', 'secrets'))
+ENV_PATH = os.path.join(CONFIG_DIR, os.getenv('ENV_FILE', '.env'))
+load_dotenv(dotenv_path=ENV_PATH, verbose=True)
+
+LOGGER = logging.getLogger(__name__)
+
+if '__main__' == __name__:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'peer_review_data.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        LOGGER.error('django.core.management not found')
+    execute_from_command_line(sys.argv)
