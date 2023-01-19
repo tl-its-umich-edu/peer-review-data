20fbb85dcd604c0583fe6bb42150dc3718f07e9a
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2023-01-19 14:42:07 -0500

    #3 - update utils to work with objects
    So `__dict__` doesn't need to be referenced for objects in the calling code, make these functions try to reference it just in case the argument is an object.

--- utils.py
+++ utils.py
@@ -1,8 +1,17 @@
-from typing import List
+from typing import List, Union
 
 
-def dictSkipKeys(d: dict, keysToSkip: List[str]) -> dict:
+def dictSkipKeys(d: Union[dict, object], keysToSkip: List[str]) -> dict:
+    try:
+        d = d.__dict__
+    except:
+        pass
     return {k: v for k, v in d.items() if k not in keysToSkip}
 
-def dictOnlyKeys(d: dict, keysToKeep: List[str]) -> dict:
+
+def dictOnlyKeys(d: Union[dict, object], keysToKeep: List[str]) -> dict:
+    try:
+        d = d.__dict__
+    except:
+        pass
     return {k: v for k, v in d.items() if k in keysToKeep}
