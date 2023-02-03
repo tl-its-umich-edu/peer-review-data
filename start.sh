5d5c9d18f232823e6916a1a69955468d7db7b59d
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2023-02-03 15:56:57 -0500

    #3 - remove example code and add debugging keep-alive code

--- start.sh
+++ start.sh
@@ -15,15 +15,8 @@ fi
 echo 'Running migrations…'
 python manage.py migrate
 
-#python manage.py loaddata "$FIXTURES_FILE"
-#
-#if [ "${TEST_MODE}" == "True" ]; then
-#    echo "Running tests"
-#    coverage run manage.py test -v 3
-#    coverage report
-#else
-#    echo "Running main placement-exams process"
-#    python manage.py run
-#fi
 echo 'Running main application…'
 python manage.py run
+
+# for debugging purposes, keep container running after job completes
+sleep 14400 # 4 hours
