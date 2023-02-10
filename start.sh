a9d51706999bd9194ab048190bf26f47a30bfcd3
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2023-02-09 22:26:28 -0500

    #10 - reformat code

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ -n "${DOCKER_COMPOSE}" ]; then
-    echo 'Checking for DB…'
-    while ! nc -z "${DB_HOST}" "${DB_PORT}"; do
-        echo 'Waiting for DB…'
-        sleep 1 # 1 second
-    done
-    echo 'DB ready.'
+  echo 'Checking for DB…'
+  while ! nc -z "${DB_HOST}" "${DB_PORT}"; do
+    echo 'Waiting for DB…'
+    sleep 1 # 1 second
+  done
+  echo 'DB ready.'
 
-    echo 'Creating migrations for model changes…'
-    python manage.py makemigrations peer_review_data
+  echo 'Creating migrations for model changes…'
+  python manage.py makemigrations peer_review_data
 fi
 
 echo 'Running migrations…'
