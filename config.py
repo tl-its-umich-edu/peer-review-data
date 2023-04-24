0b817021b4a219ca054a13553acf6f19f85a54ed
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2023-04-24 13:39:38 -0400

    #10 - improve config and Python style
    N816 actually highlighted a problem, not to be ignored.  Rather than have config export variable that's not a constant, move that variable into a function for checking the rest of the config.

--- config.py
+++ config.py
@@ -9,21 +9,32 @@ LOGGER: Logger = getLogger(__name__)
 CANVAS_BASE_URL: str = os.getenv('CANVAS_BASE_URL')
 CANVAS_API_TOKEN: str = os.getenv('CANVAS_API_TOKEN')
 COURSE_IDS_CSV: str = os.getenv('COURSE_IDS_CSV')
+COURSE_IDS: List[str] = [
+    c.strip() for c in COURSE_IDS_CSV.split(',') if c
+] if COURSE_IDS_CSV else None
 
-envErrors = []
 
-if CANVAS_BASE_URL is None:
-    envErrors.append('CANVAS_BASE_URL')
+def checkConfig():
+    envErrors = []
 
-if CANVAS_API_TOKEN is None:
-    envErrors.append('CANVAS_API_TOKEN')
+    if CANVAS_BASE_URL is None:
+        envErrors.append('CANVAS_BASE_URL')
 
-if COURSE_IDS_CSV is None:
-    envErrors.append('COURSE_IDS_CSV')
+    if CANVAS_API_TOKEN is None:
+        envErrors.append('CANVAS_API_TOKEN')
 
-if len(envErrors) > 0:
-    LOGGER.critical('The following environment variable(s) are not set: '
-                    f'{", ".join(envErrors)}')
-    sys.exit()
+    if COURSE_IDS_CSV is None:
+        envErrors.append('COURSE_IDS_CSV')
 
-COURSE_IDS: List[str] = [c.strip() for c in COURSE_IDS_CSV.split(',')]
+    if len(envErrors) > 0:
+        LOGGER.critical('The following environment variable(s) are not set: '
+                        f'{", ".join(envErrors)}')
+        sys.exit()
+
+    if COURSE_IDS is None:
+        LOGGER.critical('COURSE_IDS could not be set. '
+                        '(Problem parsing COURSE_IDS_CSV?)')
+        sys.exit()
+
+
+checkConfig()
